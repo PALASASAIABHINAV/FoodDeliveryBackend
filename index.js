53894d0ac63ec339349ffd1aa048b0f88b3e0e4c
Abhinav <psaiabhinav1724@gmail.com>
2025-11-19 23:41:49 +0530

    commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 const port = process.env.PORT || 7272;
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174","*"],
+  origin: ["http://localhost:5173", "http://localhost:5174","https://food-delivery-frontend-topaz.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
